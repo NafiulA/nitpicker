4256b62faf64a69a6f442959b88a4b1bed69f9ec
Nafiul Alam <nafiulalam30@gmail.com>
2022-03-26 11:34:18 +0600

    header component added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <nav>
+            <p className='logo'>nitpicker</p>
+        </nav>
+    );
+};
+
+export default Header;
